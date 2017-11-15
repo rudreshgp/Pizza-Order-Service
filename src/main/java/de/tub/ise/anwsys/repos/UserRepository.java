ed68e9fd5f1d90584cb1e4f6e8e55f4f04d007f1
Rudresh <gulaganjihalliparameshappa@campus.tu-berlin.de>
2017-11-15 01:29:26 +0100

    removed user object

--- src/main/java/de/tub/ise/anwsys/repos/UserRepository.java
+++ src/main/java/de/tub/ise/anwsys/repos/UserRepository.java
@@ -1,10 +1,10 @@
-package de.tub.ise.anwsys.repos;
-
-import de.tub.ise.anwsys.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<User, String> {
-    Optional<User> findByName(String name);
-}
+//package de.tub.ise.anwsys.repos;
+//
+//import de.tub.ise.anwsys.models.User;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.Optional;
+//
+//public interface UserRepository extends CrudRepository<User, String> {
+//    Optional<User> findByName(String name);
+//}
